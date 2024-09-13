402880ae70e115e87452b8b7cf9857ab4fa5c125
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2024-09-13 11:19:43 -0500

    feat(Services): Implement CsvFileHandler

--- Services/CsvFileHandler.cs
+++ Services/CsvFileHandler.cs
@@ -1,5 +1,7 @@
 using W4_assignment_template.Interfaces;
 using W4_assignment_template.Models;
+using CsvHelper;
+using System.Globalization;
 
 namespace W4_assignment_template.Services;
 
@@ -7,13 +9,31 @@ public class CsvFileHandler : IFileHandler
 {
     public List<Character> ReadCharacters(string filePath)
     {
-        // TODO: Implement CSV reading logic
-        throw new NotImplementedException();
+        List<Character> characters = [];
+
+        if (!File.Exists(filePath)) {
+            Console.WriteLine($"Character storage file does not exist, creating it at {filePath}");
+            File.Create(filePath).Dispose();
+            return characters;
+        }
+
+        using var streamReader = new StreamReader(filePath);
+        using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+
+        var records = csvReader.GetRecords<Character>().ToList();
+
+        foreach (Character character in records) {
+            characters.Add(character);
+        }
+
+        return characters;
     }
 
     public void WriteCharacters(string filePath, List<Character> characters)
     {
-        // TODO: Implement CSV writing logic
-        throw new NotImplementedException();
+        using var streamWriter = new StreamWriter(filePath);
+        using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+
+        csvWriter.WriteRecords(characters);
     }
 }
