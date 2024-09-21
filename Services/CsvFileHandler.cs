2c8902a583ad3775aa68879382ce5a84d0d33f7c
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2024-09-21 09:33:13 -0500

    fix(CsvFileHandler): use class map for saving Equipment

--- Services/CsvFileHandler.cs
+++ Services/CsvFileHandler.cs
@@ -2,22 +2,17 @@ using W4_assignment_template.Interfaces;
 using W4_assignment_template.Models;
 using CsvHelper;
 using System.Globalization;
+using CsvHelper.Configuration;
 
 namespace W4_assignment_template.Services;
 
 public class CsvFileHandler : IFileHandler
 {
-    private static string filePath = "Files/input.csv";
+    private static readonly string filePath = "Files/input.csv";
 
     public List<Character> ReadCharacters()
     {
         List<Character> characters = [];
-
-        if (!File.Exists(filePath)) {
-            Console.WriteLine($"Character storage file does not exist, creating it at {filePath}");
-            File.Create(filePath).Dispose();
-            return characters;
-        }
 
         using var streamReader = new StreamReader(filePath);
         using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
@@ -35,7 +30,24 @@ public class CsvFileHandler : IFileHandler
     {
         using var streamWriter = new StreamWriter(filePath);
         using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
-
+        
+        csvWriter.Context.RegisterClassMap<CSVMap>();
         csvWriter.WriteRecords(characters);
+    }
+
+    public CsvFileHandler() {
+        if (!File.Exists(filePath)) {
+            Console.WriteLine($"Character storage file does not exist, creating it at {filePath}");
+            File.Create(filePath).Dispose();
+        }
+    }
+}
+
+public class CSVMap : ClassMap<Character>
+{
+    public CSVMap()
+    {   
+        AutoMap(CultureInfo.InvariantCulture);
+        Map(m => m.Equipment).Convert(args => string.Join(",", args.Value.Equipment)).Index(4);
     }
 }
